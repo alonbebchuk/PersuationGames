5209d2a4b6e8591b538305e89381327df29049e7
alonbebchuk <alonbebchuk@mail.tau.ac.il>
2025-04-02 10:51:29 +0000

    Update exp.sh to include 'bert' in model_types array and uncomment model loading loops for improved data processing. This change enhances the script's functionality by allowing both BERT and Whisper models to load datasets during execution.

--- exp.sh
+++ exp.sh
@@ -1,15 +1,15 @@
-model_types=(whisper)
+model_types=(bert whisper)
 datasets=(Youtube)
 strategies=("Identity Declaration" "Accusation" "Interrogation" "Call for Action" "Defense" "Evidence")
 seeds=(12)
 
-# for model_type in ${model_types[@]}
-# do
-#   for dataset in ${datasets[@]}
-#   do
-#     python3.10 ${model_type}/load_data.py --dataset ${dataset}
-#   done
-# done
+for model_type in ${model_types[@]}
+do
+  for dataset in ${datasets[@]}
+  do
+    python3.10 ${model_type}/load_data.py --dataset ${dataset}
+  done
+done
 
 for model_type in ${model_types[@]}
 do
